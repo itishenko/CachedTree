368dcd703d991dfa062ef23994ef75e600be2421
Ivan Tishchenko <ivan.tishchenko@jjj.work>
2025-08-20 16:38:19 +0400

    Initial Commit

---
+++ CachedTree/CachedTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CachedTreeApp.swift
+//  CachedTree
+//
+//  Created by Ivan Tishchenko on 20.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct CachedTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CachedTreeTests/CachedTreeTests.swift
@@ -0,0 +1,17 @@
+//
+//  CachedTreeTests.swift
+//  CachedTreeTests
+//
+//  Created by Ivan Tishchenko on 20.08.2025.
+//
+
+import Testing
+@testable import CachedTree
+
+struct CachedTreeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
